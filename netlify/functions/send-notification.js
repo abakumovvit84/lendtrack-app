3e26e89b6bc1d17bbf6549e9964f8e91a377db61
abakumovvit84 <abakumov.vit84@gmail.com>
2026-02-18 19:03:35 +0500

    Update send-notification.js

--- netlify/functions/send-notification.js
+++ netlify/functions/send-notification.js
@@ -1,17 +1,14 @@
 exports.handler = async (event) => {
-  // Разрешаем CORS
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Methods': 'POST, OPTIONS'
   };
 
-  // Обрабатываем OPTIONS запрос (preflight)
   if (event.httpMethod === 'OPTIONS') {
     return { statusCode: 200, headers, body: '' };
   }
 
-  // Только POST запросы
   if (event.httpMethod !== 'POST') {
     return { 
       statusCode: 405, 
@@ -23,21 +20,19 @@ exports.handler = async (event) => {
   try {
     const { username, message, transaction } = JSON.parse(event.body);
     
-    // Логируем запрос (в реальном приложении здесь будет отправка в Telegram)
     console.log('📨 Уведомление:', { 
       username, 
       message, 
       transactionId: transaction.id 
     });
     
-    // Имитируем успешную отправку
     return {
       statusCode: 200,
       headers,
       body: JSON.stringify({ 
         success: true, 
         message: `Уведомление для ${username} отправлено!`,
-        test: true // Флаг что это тестовая отправка
+        test: true
       })
     };
 
